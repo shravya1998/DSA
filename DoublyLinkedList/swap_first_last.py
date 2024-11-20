1bdd0105437ceae982a08a2de6b1739d95519862
shravya1998 <40092454+shravya1998@users.noreply.github.com>
2024-11-19 23:09:53 -0600

    Created swap_first_last.py

---
+++ DoublyLinkedList/swap_first_last.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+        
+    def swap_first_last(self):
+        if self.length != 0:
+            first = self.head
+            last = self.tail
+            first.value, last.value = last.value, first.value
+        return True
+    
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
+my_doubly_linked_list.append(3)
+my_doubly_linked_list.append(4)
+
+
+print('DLL before swap_first_last():')
+my_doubly_linked_list.print_list()
+
+
+my_doubly_linked_list.swap_first_last()
+
+
+print('\nDLL after swap_first_last():')
+my_doubly_linked_list.print_list()
